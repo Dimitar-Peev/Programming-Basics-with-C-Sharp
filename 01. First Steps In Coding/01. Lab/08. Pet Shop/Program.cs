eabce8624fb8b2ca6caed8bdc12111bfe7b4b213
Dimitar-Peev <mctrix87@abv.bg>
2024-10-01 17:35:04 +0300

    Create "08. Pet Shop"

---
+++ 01. First Steps In Coding/01. Lab/08. Pet Shop/Program.cs
@@ -0,0 +1,9 @@
+﻿double dogFoodPrice = 2.50;
+double catFoodPrice = 4.00;
+
+int dogFoodPackages = int.Parse(Console.ReadLine());
+int catFoodPackages = int.Parse(Console.ReadLine());
+
+double totalSum = (dogFoodPrice * dogFoodPackages) + (catFoodPrice * catFoodPackages);
+
+Console.WriteLine($"{totalSum} lv.");
