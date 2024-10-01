7ab3bb0d93e77e98d09c7203a8b47dc533f3e856
Dimitar-Peev <mctrix87@abv.bg>
2024-10-01 17:35:30 +0300

    Create "09. Yard Greening"

---
+++ 01. First Steps In Coding/01. Lab/09. Yard Greening/Program.cs
@@ -0,0 +1,11 @@
+﻿double meters = double.Parse(Console.ReadLine());
+
+double pricePerMeter = 7.61;
+
+double sum = meters * pricePerMeter;
+
+double totalSum = sum * 0.82;
+double discount = sum * 0.18;
+
+Console.WriteLine($"The final price is: {totalSum:F2} lv.");
+Console.WriteLine($"The discount is: {discount:F2} lv.");
