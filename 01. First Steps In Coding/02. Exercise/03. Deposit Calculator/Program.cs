ef906fc1ee8513b12c0004fcb56618e0b6b8854a
Dimitar-Peev <mctrix87@abv.bg>
2024-10-01 17:38:00 +0300

    Create "03. Deposit Calculator"

---
+++ 01. First Steps In Coding/02. Exercise/03. Deposit Calculator/Program.cs
@@ -0,0 +1,7 @@
+﻿double depositSum  = double.Parse(Console.ReadLine());
+int months = int.Parse(Console.ReadLine()); 
+double percentPerYear = double.Parse(Console.ReadLine()) / 100;
+
+double totalSum = depositSum + months * ((depositSum * percentPerYear) / 12);
+
+Console.WriteLine(totalSum);
