adc00fe8ffe4383ae11c93a1b203cafa1ab0b219
Dimitar-Peev <mctrix87@abv.bg>
2024-10-02 17:04:15 +0300

    Create "04. Vacation Books List"

---
+++ 01. First Steps In Coding/02. Exercise/04. Vacation Books List/Program.cs
@@ -0,0 +1,7 @@
+﻿int totalPages = int.Parse(Console.ReadLine());
+int pagesPerHour = int.Parse(Console.ReadLine());
+int days = int.Parse(Console.ReadLine());
+
+int hours = (totalPages / pagesPerHour) / days;
+
+Console.WriteLine(hours);
