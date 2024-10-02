86b1b8b99dbd101c2ca7e4188d1af9b0af51694d
Dimitar-Peev <mctrix87@abv.bg>
2024-10-02 17:04:59 +0300

    Create "06. Repainting"

---
+++ 01. First Steps In Coding/02. Exercise/06. Repainting/Program.cs
@@ -0,0 +1,10 @@
+﻿int amountOfNylon = int.Parse(Console.ReadLine());
+int amountOfPaint = int.Parse(Console.ReadLine());
+int amountOfThinner = int.Parse(Console.ReadLine());
+int hours = int.Parse(Console.ReadLine());
+
+double sumMaterials = ((amountOfNylon + 2) * 1.5) + ((amountOfPaint + (amountOfPaint * 0.1)) * 14.5) + (amountOfThinner * 5.00) + 0.4;
+double sumWorkers = (sumMaterials * 0.3) * hours;
+double totalSum = sumMaterials + sumWorkers;
+
+Console.WriteLine(totalSum);
