ebdc3fc67cef0fc06b3ca8b858494a601ecf9193
Dimitar-Peev <mctrix87@abv.bg>
2024-10-02 17:05:28 +0300

    Create "07. Food Delivery"

---
+++ 01. First Steps In Coding/02. Exercise/07. Food Delivery/Program.cs
@@ -0,0 +1,12 @@
+﻿int chickenMenusCount = int.Parse(Console.ReadLine());  
+int fishMenusCount = int.Parse(Console.ReadLine());  
+int vegetarianMenusCount = int.Parse(Console.ReadLine());
+
+double chickenMenusPrice = 10.35;
+double fishMenusPrice = 12.40;
+double vegetarianMenusPrice = 8.15;
+
+double menusPrice = (chickenMenusCount * chickenMenusPrice) + (fishMenusCount * fishMenusPrice) + (vegetarianMenusCount * vegetarianMenusPrice);
+double totalSum =  menusPrice + (menusPrice * 0.2) + 2.50;
+
+Console.WriteLine(totalSum);
