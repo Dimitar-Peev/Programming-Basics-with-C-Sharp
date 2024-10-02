1356d255d83dfb63fb038d07565f1114636a85bd
Dimitar-Peev <mctrix87@abv.bg>
2024-10-02 17:04:38 +0300

    Create "05. Supplies for School"

---
+++ 01. First Steps In Coding/02. Exercise/05. Supplies for School/Program.cs
@@ -0,0 +1,16 @@
+﻿double penPacketPrice = 5.80;
+double markerPacketPrice = 7.20;
+double cleanerLiterPrice = 1.20;
+
+int penPacketCount = int.Parse(Console.ReadLine());
+int markerPacketCount = int.Parse(Console.ReadLine());
+int cleanerLitersCount = int.Parse(Console.ReadLine());
+int percentDiscount = int.Parse(Console.ReadLine());
+
+double sum = (penPacketCount * penPacketPrice) 
+          + (markerPacketCount * markerPacketPrice) 
+          + (cleanerLitersCount * cleanerLiterPrice);
+
+double totalSum = sum - (sum * percentDiscount / 100);
+
+Console.WriteLine(totalSum);
