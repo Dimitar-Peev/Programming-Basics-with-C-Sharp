80d02013dcea59b813cb92371eabad67e2d4dd06
Dimitar-Peev <mctrix87@abv.bg>
2024-10-02 17:05:53 +0300

    Create "08. Basketball Equipment"

---
+++ 01. First Steps In Coding/02. Exercise/08. Basketball Equipment/Program.cs
@@ -0,0 +1,10 @@
+﻿int pricePerYear = int.Parse(Console.ReadLine());
+
+double shoesPrice = pricePerYear * 0.6;
+double clothesPrice = shoesPrice * 0.8;
+double ballPrice = clothesPrice / 4;
+double accessoriesPrice = ballPrice / 5;
+
+double totalPrice = pricePerYear + shoesPrice + clothesPrice + ballPrice + accessoriesPrice;
+
+Console.WriteLine(totalPrice);
