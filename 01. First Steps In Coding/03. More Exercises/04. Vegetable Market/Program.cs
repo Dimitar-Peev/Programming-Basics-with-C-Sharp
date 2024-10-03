22f7d8d0b9f791237ccdc7a1f323e85ad6bd3da5
Dimitar-Peev <mctrix87@abv.bg>
2024-10-03 17:56:21 +0300

    Create "04. Vegetable Market"

---
+++ 01. First Steps In Coding/03. More Exercises/04. Vegetable Market/Program.cs
@@ -0,0 +1,11 @@
+﻿double priceForKgVegetables = double.Parse(Console.ReadLine());
+double priceForKgFruits = double.Parse(Console.ReadLine());
+int sumForKgVegetables = int.Parse(Console.ReadLine());
+int sumForKgFruits = int.Parse(Console.ReadLine());
+
+double kgVegetables = priceForKgVegetables * sumForKgVegetables;
+double kgFruits = priceForKgFruits * sumForKgFruits;
+double sum = kgVegetables + kgFruits;
+double euro = sum * 0.5154639175;
+
+Console.WriteLine($"{euro:F2}");
