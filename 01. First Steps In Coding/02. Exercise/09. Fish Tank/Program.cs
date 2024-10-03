dd08c6a4be194b541e49c2ec610d2e6966aa37be
Dimitar-Peev <mctrix87@abv.bg>
2024-10-03 17:53:41 +0300

    Create "09. Fish Tank"

---
+++ 01. First Steps In Coding/02. Exercise/09. Fish Tank/Program.cs
@@ -0,0 +1,8 @@
+﻿int length = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+double percent = double.Parse(Console.ReadLine()) / 100;
+
+double liters = ((length * width * height) * 0.001) * (1 - percent);
+
+Console.WriteLine(liters);
