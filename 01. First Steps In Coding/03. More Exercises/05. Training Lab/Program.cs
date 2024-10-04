a8605bbbe31df56e4fd5cc6547c89ca034e7e486
Dimitar-Peev <mctrix87@abv.bg>
2024-10-04 20:38:02 +0300

    Create "05. Training Lab"

---
+++ 01. First Steps In Coding/03. More Exercises/05. Training Lab/Program.cs
@@ -0,0 +1,8 @@
+﻿double w = double.Parse(Console.ReadLine()) * 100;
+double h = double.Parse(Console.ReadLine()) * 100;
+
+double freeSpaceW = Math.Floor(w / 120);
+double freeSpaceH = Math.Floor((h - 100) / 70);
+double countWorkSpace = (freeSpaceH * freeSpaceW) - 3;
+
+Console.WriteLine($"{countWorkSpace:F0}");
