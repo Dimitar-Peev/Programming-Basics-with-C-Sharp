787329206c83ed36133dabeffb5c195b06567266
Dimitar-Peev <mctrix87@abv.bg>
2024-10-04 20:39:08 +0300

    Create "07. House Painting"

---
+++ 01. First Steps In Coding/03. More Exercises/07. House Painting/Program.cs
@@ -0,0 +1,11 @@
+﻿double x = double.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+double h = double.Parse(Console.ReadLine());
+
+double greenArea = 2 * (x * y) - 2 * (1.5 * 1.5) + 2 * (x * x) - 1.2 * 2;
+double greenPaint = greenArea / 3.4;
+double redArea = 2 * (x * y) + 2 * ((x * h) / 2);
+double redPaint = redArea / 4.3;
+
+Console.WriteLine($"{greenPaint:F2}");
+Console.WriteLine($"{redPaint:F2}");
