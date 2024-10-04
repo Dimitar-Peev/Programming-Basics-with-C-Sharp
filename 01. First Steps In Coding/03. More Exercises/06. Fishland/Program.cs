2141a14fd471ca97bc20ef327f3dc2ae1a96294b
Dimitar-Peev <mctrix87@abv.bg>
2024-10-04 20:38:37 +0300

    Create "06. Fishland"

---
+++ 01. First Steps In Coding/03. More Exercises/06. Fishland/Program.cs
@@ -0,0 +1,13 @@
+﻿double mackerelPricePerKg = double.Parse(Console.ReadLine());
+double sprinklePricePerKg = double.Parse(Console.ReadLine());
+double bonitoKg = double.Parse(Console.ReadLine());
+double safridKg = double.Parse(Console.ReadLine());
+int mussels = int.Parse(Console.ReadLine());
+
+double bonitoPriceKg = (mackerelPricePerKg * 1.6) * bonitoKg;
+double safridPriceKg = (sprinklePricePerKg * 1.8) * safridKg;
+double musselsPriceKg = mussels * 7.5;
+
+double totalSum = bonitoPriceKg + safridPriceKg + musselsPriceKg;
+
+Console.WriteLine($"{totalSum:F2}");
