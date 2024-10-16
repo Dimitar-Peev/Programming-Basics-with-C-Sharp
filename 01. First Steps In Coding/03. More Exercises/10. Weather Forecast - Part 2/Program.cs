909848ba09bff4e26eca7cf779f8e2e9ed5e926b
Dimitar-Peev <mctrix87@abv.bg>
2024-10-16 19:50:55 +0300

    Create "10. Weather Forecast - Part 2"

---
+++ 01. First Steps In Coding/03. More Exercises/10. Weather Forecast - Part 2/Program.cs
@@ -0,0 +1,26 @@
+﻿double celsius = double.Parse(Console.ReadLine());
+
+if (celsius >= 26 && celsius <= 35)
+{
+    Console.WriteLine("Hot");
+}
+else if (celsius >= 20.1 && celsius <= 25.9)
+{
+    Console.WriteLine("Warm");
+}
+else if (celsius >= 15.00 && celsius <= 20.00)
+{
+    Console.WriteLine("Mild");
+}
+else if (celsius >= 12.00 && celsius <= 14.9)
+{
+    Console.WriteLine("Cool");
+}
+else if (celsius >= 5.00 && celsius <= 11.9)
+{
+    Console.WriteLine("Cold");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
