03c578b4e28bd5aa73ac53125ca77939d1370be3
Dimitar-Peev <mctrix87@abv.bg>
2024-10-21 23:47:25 +0300

    Create "07. Area of Figures"

---
+++ 02. Conditional Statements/01. Lab/07. Area of Figures/Program.cs
@@ -0,0 +1,31 @@
+﻿string typeFigure = Console.ReadLine();
+
+double area = 0;
+
+if ("square".Equals(typeFigure))
+{
+    double side = double.Parse(Console.ReadLine());
+    area = side * side;
+
+}
+else if ("rectangle".Equals(typeFigure))
+{
+    double a = double.Parse(Console.ReadLine());
+    double b = double.Parse(Console.ReadLine());
+    area = a * b;
+
+}
+else if ("circle".Equals(typeFigure))
+{
+    double radius = double.Parse(Console.ReadLine());
+    area = radius * radius * Math.PI;
+
+}
+else if ("triangle".Equals(typeFigure))
+{
+    double length = double.Parse(Console.ReadLine());
+    double height = double.Parse(Console.ReadLine());
+    area = (length * height) / 2;
+
+}
+Console.WriteLine($"{area:F3}");
