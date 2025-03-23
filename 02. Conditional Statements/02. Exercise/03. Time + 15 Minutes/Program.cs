664fca9d9e2cf6bbe3dcb537c03c46cb8733791b
Dimitar-Peev <mctrix87@abv.bg>
2025-03-23 20:14:40 +0200

    Create "03. Time + 15 Minutes"

---
+++ 02. Conditional Statements/02. Exercise/03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,28 @@
+﻿int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+if (minutes >= 45)
+{
+    hours++;
+    minutes = (minutes + 15) % 60;
+}
+else
+{
+    minutes += 15;
+}
+
+
+if (hours == 24)
+{
+    hours = 0;
+}
+
+
+if (minutes < 10)
+{
+    Console.WriteLine($"{hours}:0{minutes}");
+}
+else
+{
+    Console.WriteLine($"{hours}:{minutes}");
+}
