ee3081a0b867425acebf87ef6badbd42b955c35e
Dimitar-Peev <mctrix87@abv.bg>
2025-03-24 18:10:09 +0200

    Create "06. World Swimming Record"

---
+++ 02. Conditional Statements/02. Exercise/06. World Swimming Record/Program.cs
@@ -0,0 +1,18 @@
+﻿double recordsInSeconds = double.Parse(Console.ReadLine());
+double metersToSwim = double.Parse(Console.ReadLine());
+double secondsForOneMeterSwim = double.Parse(Console.ReadLine());
+
+double swimSecondsWithoutDelay = metersToSwim * secondsForOneMeterSwim;
+double delayTimes = Math.Floor(metersToSwim / 15);
+
+double swimSeconds = swimSecondsWithoutDelay + (delayTimes * 12.5);
+
+if (swimSeconds < recordsInSeconds)
+{
+    Console.WriteLine($"Yes, he succeeded! The new world record is {swimSeconds:F2} seconds.");
+}
+else
+{
+    double neededSeconds = swimSeconds - recordsInSeconds;
+    Console.WriteLine($"No, he failed! He was {neededSeconds:F2} seconds slower.");
+}
