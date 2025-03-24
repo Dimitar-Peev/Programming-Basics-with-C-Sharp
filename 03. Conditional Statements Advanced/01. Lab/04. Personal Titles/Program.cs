b741237b8a5faba79f368f90fee2c57d8daf1b5d
Dimitar-Peev <mctrix87@abv.bg>
2025-03-24 18:37:22 +0200

    Create "04. Personal Titles"

---
+++ 03. Conditional Statements Advanced/01. Lab/04. Personal Titles/Program.cs
@@ -0,0 +1,26 @@
+﻿double age = double.Parse(Console.ReadLine());
+char gender = char.Parse(Console.ReadLine());
+
+switch (gender)
+{
+    case 'm':
+        if (age >= 16)
+        {
+            Console.WriteLine("Mr.");
+        }
+        else
+        {
+            Console.WriteLine("Master");
+        }
+        break;
+    case 'f':
+        if (age >= 16)
+        {
+            Console.WriteLine("Ms.");
+        }
+        else
+        {
+            Console.WriteLine("Miss");
+        }
+        break;
+}
