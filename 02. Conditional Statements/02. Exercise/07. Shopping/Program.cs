72b9eb72310cc22bf894326117149dc3bf8ae85f
Dimitar-Peev <mctrix87@abv.bg>
2025-03-24 18:10:38 +0200

    Create "07. Shopping"

---
+++ 02. Conditional Statements/02. Exercise/07. Shopping/Program.cs
@@ -0,0 +1,26 @@
+﻿double budget = double.Parse(Console.ReadLine());
+int videoCardsCount = int.Parse(Console.ReadLine());
+int processorsCount = int.Parse(Console.ReadLine());
+int ramsCount = int.Parse(Console.ReadLine());
+
+double videocardsBill = videoCardsCount * 250;
+double processorsBill = processorsCount * (videocardsBill * 0.35);
+double ramsBill = ramsCount * (videocardsBill * 0.1);
+
+double bill = videocardsBill + processorsBill + ramsBill;
+
+if (videoCardsCount > processorsCount)
+{
+    bill *= 0.85;
+}
+
+if (budget >= bill)
+{
+    double moneyLeft = budget - bill;
+    Console.WriteLine($"You have {moneyLeft:F2} leva left!");
+}
+else
+{
+    double neededMoney = bill - budget;
+    Console.WriteLine($"Not enough money! You need {neededMoney:F2} leva more!");
+}
