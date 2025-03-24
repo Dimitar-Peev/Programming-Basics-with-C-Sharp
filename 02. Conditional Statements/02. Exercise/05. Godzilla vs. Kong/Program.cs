9361e326f23a6f3e2b98ba378e73b72de624f7a2
Dimitar-Peev <mctrix87@abv.bg>
2025-03-24 18:04:41 +0200

    Create "05. Godzilla vs. Kong"

---
+++ 02. Conditional Statements/02. Exercise/05. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,25 @@
+﻿double budget = double.Parse(Console.ReadLine());
+int numOfPeople = int.Parse(Console.ReadLine());
+double pricePerPerson = double.Parse(Console.ReadLine());
+
+double decor = budget * 0.1;
+double costumerPrice = pricePerPerson * numOfPeople;
+
+if (numOfPeople > 150)
+{
+    costumerPrice = costumerPrice - costumerPrice * 0.1;
+}
+
+double result = decor + costumerPrice;
+double diff = Math.Abs(budget - result);
+
+if (budget >= result)
+{
+    Console.WriteLine("Action!");
+    Console.WriteLine($"Wingard starts filming with {diff:F2} leva left.");
+}
+else
+{
+    Console.WriteLine("Not enough money!");
+    Console.WriteLine($"Wingard needs {diff:F2} leva more.", diff);
+}
