613117ceab42643b36fe4fdffa9ed1d705289dc4
Dimitar-Peev <mctrix87@abv.bg>
2025-03-25 17:55:21 +0200

    Create "02. Half Sum Element"

---
+++ 04. For Loop/02. Exercise/02. Half Sum Element/Program.cs
@@ -0,0 +1,29 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+int maxNumber = int.MinValue;
+
+for (int i = 0; i < n; i++)
+{
+    int currentNumber = int.Parse(Console.ReadLine());
+    sum += currentNumber;
+
+    if (maxNumber < currentNumber)
+    {
+        maxNumber = currentNumber;
+    }
+}
+
+sum -= maxNumber;
+
+if (maxNumber == sum)
+{
+    Console.WriteLine("Yes");
+    Console.WriteLine($"Sum = {sum}");
+}
+else
+{
+    int difference = Math.Abs(maxNumber - sum);
+    Console.WriteLine("No");
+    Console.WriteLine($"Diff = {difference}");
+}
