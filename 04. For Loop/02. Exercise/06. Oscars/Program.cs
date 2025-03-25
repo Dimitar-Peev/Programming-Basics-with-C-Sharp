96f694f7fd32e5dbaf6784e7f703eaa9e93b4bd4
Dimitar-Peev <mctrix87@abv.bg>
2025-03-25 18:16:33 +0200

    Create "06. Oscars"

---
+++ 04. For Loop/02. Exercise/06. Oscars/Program.cs
@@ -0,0 +1,25 @@
+﻿string actorName = Console.ReadLine();
+double points = double.Parse(Console.ReadLine());
+int numOfJudges = int.Parse(Console.ReadLine());
+
+double pointsForNomination = 1250.5;
+
+for (int i = 0; i < numOfJudges; i++)
+{
+    string currentJudgeName = Console.ReadLine();
+    double currentJudgePoints = double.Parse(Console.ReadLine());
+
+    points += (currentJudgeName.Length * currentJudgePoints) / 2;
+
+    if (points > pointsForNomination)
+    {
+        Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {points:f1}!");
+        break;
+    }
+}
+
+if (points <= pointsForNomination)
+{
+    double neededPoints = pointsForNomination - points;
+    Console.WriteLine($"Sorry, {actorName} you need {neededPoints:f1} more!");
+}
