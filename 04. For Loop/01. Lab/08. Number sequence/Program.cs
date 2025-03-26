ae3faab2c2a962130f15d6434fb01d1896c0b310
Dimitar-Peev <mctrix87@abv.bg>
2025-03-26 22:40:51 +0200

    Create "08. Number sequence"

---
+++ 04. For Loop/01. Lab/08. Number sequence/Program.cs
@@ -0,0 +1,24 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int maxNum = 0;
+int minNum = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+
+    if (num > maxNum)
+    {
+        maxNum = num;
+
+    }
+
+    if (num < minNum)
+    {
+        minNum = num;
+
+    }
+}
+
+Console.WriteLine($"Max number: {maxNum}");
+Console.WriteLine($"Min number: {minNum}");
