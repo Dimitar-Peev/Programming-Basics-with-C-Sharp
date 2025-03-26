4177f6deee478e74ac1fe18adfb9c0d224e09346
Dimitar-Peev <mctrix87@abv.bg>
2025-03-26 22:45:58 +0200

    Create "10. Odd Even Sum"

---
+++ 04. For Loop/01. Lab/10. Odd Even Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int oddSum = 0;
+int evenSum = 0;
+
+for (int i = 0; i < n; i++)
+{
+    if (i % 2 == 0)
+    {
+        evenSum += int.Parse(Console.ReadLine());
+
+    }
+    else
+    {
+        oddSum += int.Parse(Console.ReadLine());
+
+    }
+}
+
+if (evenSum == oddSum)
+{
+    Console.WriteLine("Yes");
+    Console.WriteLine($"Sum = {evenSum}");
+}
+else
+{
+    int diff = Math.Abs(evenSum - oddSum);
+    Console.WriteLine("No");
+    Console.WriteLine($"Diff = {diff}");
+}
