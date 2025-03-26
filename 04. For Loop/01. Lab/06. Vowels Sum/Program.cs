b9c6eb1eb6bf622959d7517d5f1210d10ee51efc
Dimitar-Peev <mctrix87@abv.bg>
2025-03-26 22:35:27 +0200

    Create "06. Vowels Sum"

---
+++ 04. For Loop/01. Lab/06. Vowels Sum/Program.cs
@@ -0,0 +1,18 @@
+﻿string input = Console.ReadLine();
+int points = 0;
+
+int length = input.Length;
+
+for (int i = 0; i < length; i++)
+{
+    switch (input[i])
+    {
+        case 'a': points += 1; break;
+        case 'e': points += 2; break;
+        case 'i': points += 3; break;
+        case 'o': points += 4; break;
+        case 'u': points += 5; break;
+    }
+}
+
+Console.WriteLine(points);
