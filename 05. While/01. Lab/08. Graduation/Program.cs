9807d9aeee6bb8ba2f70714bc3c60a0677a8ecb3
Dimitar-Peev <mctrix87@abv.bg>
2025-03-27 18:56:55 +0200

    Create "08. Graduation"

---
+++ 05. While/01. Lab/08. Graduation/Program.cs
@@ -0,0 +1,34 @@
+﻿string studentName = Console.ReadLine();
+
+int course = 0;
+int fails = 0;
+double sum = 0;
+
+while (course < 12)
+{
+    double grade = double.Parse(Console.ReadLine());
+    if (grade < 4)
+    {
+        if (fails == 1)
+        {
+            break;
+        }
+
+        fails++;
+    }
+    else
+    {
+        sum += grade;
+        course++;
+        fails = 0;
+    }
+}
+
+if (course == 12)
+{
+    Console.WriteLine($"{studentName} graduated. Average grade: {sum / 12:f2}");
+}
+else
+{
+    Console.WriteLine($"{studentName} has been excluded at {course + 1} grade");
+}
