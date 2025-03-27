6b09efaa137963ff49a949b18dd1b764f5ea07ff
Dimitar-Peev <mctrix87@abv.bg>
2025-03-27 18:44:18 +0200

    Create "05. Account Balance"

---
+++ 05. While/01. Lab/05. Account Balance/Program.cs
@@ -0,0 +1,19 @@
+﻿double amount = 0;
+
+string input = Console.ReadLine();
+while ("NoMoreMoney" != input)
+{
+    double number = double.Parse(input);
+
+    if (number < 0)
+    {
+        Console.WriteLine("Invalid operation!");
+        break;
+    }
+
+    amount += number;
+    Console.WriteLine($"Increase: {number:f2}");
+    input = Console.ReadLine();
+}
+
+Console.WriteLine($"Total: {amount:f2}");
