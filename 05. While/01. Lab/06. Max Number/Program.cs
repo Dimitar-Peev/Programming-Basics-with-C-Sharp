e4154f7c14763b12a9c43ee1cd6360411ce8beef
Dimitar-Peev <mctrix87@abv.bg>
2025-03-27 18:47:49 +0200

    Create "06. Max Number"

---
+++ 05. While/01. Lab/06. Max Number/Program.cs
@@ -0,0 +1,16 @@
+﻿int max = int.MinValue;
+
+string input = Console.ReadLine();
+while ("Stop" != input)
+{
+    int currentNumber = int.Parse(input);
+
+    if (currentNumber > max)
+    {
+        max = currentNumber;
+    }
+
+    input = Console.ReadLine();
+}
+
+Console.WriteLine(max);
