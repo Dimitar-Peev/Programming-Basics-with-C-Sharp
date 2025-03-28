96ac48186c2f88b3bc444fded2a3dba756f57bc1
Dimitar-Peev <mctrix87@abv.bg>
2025-03-28 17:34:15 +0200

    Create "02. Exam Preparation"

---
+++ 05. While/02. Exercise/02. Exam Preparation/Program.cs
@@ -0,0 +1,40 @@
+﻿int poorGradesLimit = int.Parse(Console.ReadLine());
+
+int problemsCount = 0;
+int poorGradesCount = 0;
+int allGradesSum = 0;
+string breakCommand = "Enough";
+string lastProblem = "";
+
+string problemName = Console.ReadLine();
+
+while (breakCommand != problemName)
+{
+    int currentGrade = int.Parse(Console.ReadLine());
+    problemsCount++;
+    allGradesSum += currentGrade;
+    lastProblem = problemName;
+
+    if (currentGrade <= 4)
+    {
+        poorGradesCount++;
+
+        if (poorGradesCount == poorGradesLimit)
+        {
+            Console.WriteLine($"You need a break, {poorGradesLimit} poor grades.");
+            break;
+        }
+    }
+
+    problemName = Console.ReadLine();
+}
+
+
+if (poorGradesCount < poorGradesLimit)
+{
+    double averageGrade = (double)allGradesSum / problemsCount;
+
+    Console.WriteLine($"Average score: {averageGrade:F2}");
+    Console.WriteLine($"Number of problems: {problemsCount}");
+    Console.WriteLine($"Last problem: {lastProblem}");
+}
