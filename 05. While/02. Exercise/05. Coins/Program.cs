c051569f1a9019b5206da0b67c3e28e9f0886e89
Dimitar-Peev <mctrix87@abv.bg>
2025-03-28 17:54:35 +0200

    Create "05. Coins"

---
+++ 05. While/02. Exercise/05. Coins/Program.cs
@@ -0,0 +1,59 @@
+﻿//Input
+double inputChange = double.Parse(Console.ReadLine());
+
+double sum = Math.Round(inputChange * 100);
+int coinsCounter = 0;
+
+while (sum > 0)
+{
+    if (sum - 200 >= 0)
+    {
+        sum -= 200;
+        coinsCounter++;
+        continue;
+    }
+    else if (sum - 100 >= 0)
+    {
+        sum -= 100;
+        coinsCounter++;
+        continue;
+    }
+    else if (sum - 50 >= 0)
+    {
+        sum -= 50;
+        coinsCounter++;
+        continue;
+    }
+    else if (sum - 20 >= 0)
+    {
+        sum -= 20;
+        coinsCounter++;
+        continue;
+    }
+    else if (sum - 10 >= 0)
+    {
+        sum -= 10;
+        coinsCounter++;
+        continue;
+    }
+    else if (sum - 5 >= 0)
+    {
+        sum -= 5;
+        coinsCounter++;
+    }
+    else if (sum - 2 >= 0)
+    {
+        sum -= 2;
+        coinsCounter++;
+        continue;
+    }
+    else if (sum - 1 >= 0)
+    {
+        sum -= 1;
+        coinsCounter++;
+        continue;
+    }
+}
+
+//Output
+Console.WriteLine(coinsCounter);
