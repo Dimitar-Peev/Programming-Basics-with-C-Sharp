808479f4fcec3ac649ca199effef21790e3f4634
Dimitar-Peev <mctrix87@abv.bg>
2025-03-28 18:00:15 +0200

    Create "06. Cake"

---
+++ 05. While/02. Exercise/06. Cake/Program.cs
@@ -0,0 +1,26 @@
+﻿int width = int.Parse(Console.ReadLine());
+int length = int.Parse(Console.ReadLine());
+
+int cakePieces = width * length;
+string breakCommand = "STOP";
+
+string input = Console.ReadLine();
+
+while (breakCommand != input)
+{
+    int currentPieces = int.Parse(input);
+    cakePieces -= currentPieces;
+
+    if (cakePieces <= 0)
+    {
+        Console.WriteLine($"No more cake left! You need {Math.Abs(cakePieces)} pieces more.");
+        break;
+    }
+
+    input = Console.ReadLine();
+}
+
+if (cakePieces > 0)
+{
+    Console.WriteLine($"{cakePieces} pieces are left.");
+}
