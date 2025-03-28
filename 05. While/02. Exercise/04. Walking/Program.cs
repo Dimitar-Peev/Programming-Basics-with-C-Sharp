9ee1315fd0a6652a47de5cc9daa10d9dc5c69253
Dimitar-Peev <mctrix87@abv.bg>
2025-03-28 17:51:20 +0200

    Create "04. Walking"

---
+++ 05. While/02. Exercise/04. Walking/Program.cs
@@ -0,0 +1,29 @@
+﻿int stepsCount = 0;
+int stepsGoal = 10000;
+string breakCommand = "Going home";
+
+while (stepsCount < stepsGoal)
+{
+    string input = Console.ReadLine();
+
+    if (input == breakCommand)
+    {
+        int stepsToHome = int.Parse(Console.ReadLine());
+        stepsCount += stepsToHome;
+        break;
+    }
+
+    int currentSteps = int.Parse(input);
+    stepsCount += currentSteps;
+}
+
+int diffSteps = Math.Abs(stepsCount - stepsGoal);
+if (stepsCount >= stepsGoal)
+{
+    Console.WriteLine("Goal reached! Good job!");
+    Console.WriteLine($"{diffSteps} steps over the goal!");
+}
+else
+{
+    Console.WriteLine($"{diffSteps} more steps to reach goal.");
+}
