581235d76762c18afeaf3fd30085dc68675e7bd9
Dimitar-Peev <mctrix87@abv.bg>
2025-03-28 17:48:03 +0200

    Create "03. Vacation"

---
+++ 05. While/02. Exercise/03. Vacation/Program.cs
@@ -0,0 +1,41 @@
+﻿double tripMoney = double.Parse(Console.ReadLine());
+double savedMoney = double.Parse(Console.ReadLine());
+
+int allDaysCounter = 0;
+int spendDaysInRowCounter = 0;
+
+while (savedMoney < tripMoney)
+{
+    string action = Console.ReadLine();
+    double currentMoney = double.Parse(Console.ReadLine());
+
+    allDaysCounter++;
+
+    if (action == "spend")
+    {
+        spendDaysInRowCounter++;
+        savedMoney -= currentMoney;
+
+        if (savedMoney < 0)
+        {
+            savedMoney = 0;
+        }
+
+        if (spendDaysInRowCounter == 5)
+        {
+            Console.WriteLine("You can't save the money.");
+            Console.WriteLine(allDaysCounter);
+            break;
+        }
+    }
+    else if (action == "save")
+    {
+        spendDaysInRowCounter = 0;
+        savedMoney += currentMoney;
+    }
+}
+
+if (savedMoney >= tripMoney)
+{
+    Console.WriteLine($"You saved the money for {allDaysCounter} days.");
+}
