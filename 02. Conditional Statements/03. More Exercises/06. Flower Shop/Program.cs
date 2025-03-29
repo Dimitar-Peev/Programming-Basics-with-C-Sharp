df5cd945a8a1e52aa854a0314757dde7aedf2635
Dimitar-Peev <mctrix87@abv.bg>
2025-03-29 13:12:49 +0200

    Create "06. Flower Shop"

---
+++ 02. Conditional Statements/03. More Exercises/06. Flower Shop/Program.cs
@@ -0,0 +1,16 @@
+﻿int magnolias = int.Parse(Console.ReadLine());
+int hyacinths = int.Parse(Console.ReadLine());
+int roses = int.Parse(Console.ReadLine());
+int cacti = int.Parse(Console.ReadLine());
+double giftPrice = double.Parse(Console.ReadLine());
+
+double profit = ((magnolias * 3.25) + (hyacinths * 4) + (roses * 3.5) + (cacti * 8)) * 0.95;
+
+if (profit >= giftPrice)
+{
+    Console.WriteLine($"She is left with {Math.Floor(profit - giftPrice):F0} leva.");
+}
+else
+{
+    Console.WriteLine($"She will have to borrow {Math.Ceiling(giftPrice - profit):F0} leva.");
+}
