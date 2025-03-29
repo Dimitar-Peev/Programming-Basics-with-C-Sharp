a91e0ce3ba475b4faf71700b91b564774a5ab190
Dimitar-Peev <mctrix87@abv.bg>
2025-03-29 13:09:45 +0200

    Create "05. Pets"

---
+++ 02. Conditional Statements/03. More Exercises/05. Pets/Program.cs
@@ -0,0 +1,16 @@
+﻿int days = int.Parse(Console.ReadLine());
+int foodInKg = int.Parse(Console.ReadLine());
+double dogFoodKg = double.Parse(Console.ReadLine());
+double catFoodKg = double.Parse(Console.ReadLine());
+double turtleFoodGrams = double.Parse(Console.ReadLine());
+
+double neededFood = (days * dogFoodKg) + (days * catFoodKg) + (days * turtleFoodGrams / 1000);
+
+if (foodInKg >= neededFood)
+{
+    Console.WriteLine($"{Math.Floor(foodInKg - neededFood):F0} kilos of food left.");
+}
+else
+{
+    Console.WriteLine($"{Math.Ceiling(neededFood - foodInKg):F0} more kilos of food are needed.");
+}
