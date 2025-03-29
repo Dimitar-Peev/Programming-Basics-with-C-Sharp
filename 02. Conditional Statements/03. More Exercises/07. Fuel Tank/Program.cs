80a58fffc5e18f449d7260255eb92e12ef26973f
Dimitar-Peev <mctrix87@abv.bg>
2025-03-29 13:16:13 +0200

    Create "07. Fuel Tank"

---
+++ 02. Conditional Statements/03. More Exercises/07. Fuel Tank/Program.cs
@@ -0,0 +1,43 @@
+﻿string fuelType = Console.ReadLine();
+int fuleQuantity = int.Parse(Console.ReadLine());
+string outputMessage = "";
+
+switch (fuelType)
+{
+    case "Diesel":
+        if (fuleQuantity >= 25)
+        {
+            outputMessage = "You have enough diesel.";
+        }
+        else
+        {
+            outputMessage = "Fill your tank with diesel!";
+        }
+        break;
+    case "Gasoline":
+        if (fuleQuantity >= 25)
+        {
+            outputMessage = "You have enough gasoline.";
+        }
+        else
+        {
+            outputMessage = "Fill your tank with gasoline!";
+        }
+        break;
+    case "Gas":
+        if (fuleQuantity >= 25)
+        {
+            outputMessage = "You have enough gas.";
+        }
+        else
+        {
+            outputMessage = "Fill your tank with gas!";
+        }
+        break;
+    default:
+        outputMessage = "Invalid fuel!";
+        break;
+
+}
+
+Console.WriteLine(outputMessage);
