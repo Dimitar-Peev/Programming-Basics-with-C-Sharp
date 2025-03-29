3f7930b96e70cec06cbd4cc3720be800f1fb3d55
Dimitar-Peev <mctrix87@abv.bg>
2025-03-29 13:19:39 +0200

    Create "08. Fuel Tank - Part 2"

---
+++ 02. Conditional Statements/03. More Exercises/08. Fuel Tank - Part 2/Program.cs
@@ -0,0 +1,54 @@
+﻿string fuelType = Console.ReadLine();
+int fuelQuantity = int.Parse(Console.ReadLine());
+string clubCard = Console.ReadLine();
+
+double fuelPrice = 0;
+
+switch (fuelType)
+{
+    case "Gas":
+        if (clubCard.Equals("Yes"))
+        {
+            fuelPrice = 0.93 - 0.08;
+        }
+        else if (clubCard.Equals("No"))
+        {
+            fuelPrice = 0.93;
+        }
+        break;
+    case "Gasoline":
+        if (clubCard.Equals("Yes"))
+        {
+            fuelPrice = 2.22 - 0.18;
+        }
+        else if (clubCard.Equals("No"))
+        {
+            fuelPrice = 2.22;
+        }
+        break;
+    case "Diesel":
+        if (clubCard.Equals("Yes"))
+        {
+            fuelPrice = 2.33 - 0.12;
+        }
+        else if (clubCard.Equals("No"))
+        {
+            fuelPrice = 2.33;
+        }
+        break;
+}
+
+double totalPrice = fuelPrice * fuelQuantity;
+
+if (fuelQuantity >= 20 && fuelQuantity <= 25)
+{
+    totalPrice = totalPrice * 0.92;
+}
+else if (fuelQuantity > 25)
+{
+    totalPrice = totalPrice * 0.9;
+}
+
+
+Console.WriteLine($"{totalPrice:F2} lv.");
+
