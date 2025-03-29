d67e2da74e87d5a3827b26035a78effc23817300
Dimitar-Peev <mctrix87@abv.bg>
2025-03-29 12:51:46 +0200

    Create "04. Transport Price"

---
+++ 02. Conditional Statements/03. More Exercises/04. Transport Price/Program.cs
@@ -0,0 +1,32 @@
+﻿int kilometers = int.Parse(Console.ReadLine());
+string dayTime = Console.ReadLine();
+
+double taxiStartPrice = 0.70;
+double taxiDaylyRate = 0.79;
+double taxiNightRate = 0.90;
+double bussPrice = 0.09;
+double trainPrice = 0.06;
+int trainMinDistance = 100;
+double price = 0.0;
+
+if (kilometers < 20)
+{
+    if (dayTime.Equals("day"))
+    {
+        price = taxiStartPrice + (kilometers * taxiDaylyRate);
+    }
+    else if (dayTime.Equals("night"))
+    {
+        price = taxiStartPrice + (kilometers * taxiNightRate);
+    }
+}
+else if (kilometers < trainMinDistance)
+{
+    price = kilometers * bussPrice;
+}
+else
+{
+    price = kilometers * trainPrice;
+}
+
+Console.WriteLine($"{price:F2}", price);
