568d17237ab5abe11d0dcbfa2a47ca7e4a9a85b5
Dimitar-Peev <mctrix87@abv.bg>
2025-03-29 12:49:37 +0200

    Create "02. Sleepy Tom Cat"

---
+++ 02. Conditional Statements/03. More Exercises/02. Sleepy Tom Cat/Program.cs
@@ -0,0 +1,21 @@
+﻿int restDays = int.Parse(Console.ReadLine());
+
+int gameMinutesPerYear = 30000;
+int minutesForGameInWorkingDay = 63;
+int minutesForGameInDayOff = 127;
+
+int workingDays = 365 - restDays;
+int realGameMinutes = workingDays * minutesForGameInWorkingDay + restDays * minutesForGameInDayOff;
+int hours = Math.Abs(gameMinutesPerYear - realGameMinutes) / 60;
+int minutes = Math.Abs(gameMinutesPerYear - realGameMinutes) % 60;
+
+if (realGameMinutes > gameMinutesPerYear)
+{
+    Console.WriteLine("Tom will run away");
+    Console.WriteLine($"{hours} hours and {minutes} minutes more for play");
+}
+else
+{
+    Console.WriteLine("Tom sleeps well");
+    Console.WriteLine($"{hours} hours and {minutes} minutes less for play");
+}
