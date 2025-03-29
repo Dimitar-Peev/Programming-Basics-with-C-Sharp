56bd2c7b64b2ab36ee71d6608940e32eee1e33c8
Dimitar-Peev <mctrix87@abv.bg>
2025-03-29 12:47:52 +0200

    Create "03. Harvest"

---
+++ 02. Conditional Statements/03. More Exercises/03. Harvest/Program.cs
@@ -0,0 +1,19 @@
+﻿int x = int.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+int zLitters = int.Parse(Console.ReadLine());
+int workers = int.Parse(Console.ReadLine());
+
+double grapes = x * y;
+double wine = (0.4 * grapes) / 2.5;
+double diff = Math.Abs(wine - zLitters);
+
+if (wine >= zLitters)
+{
+    double profitPerPerson = diff / workers;
+    Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(wine):F0} liters.");
+    Console.WriteLine($"{Math.Ceiling(diff):F0} liters left -> {Math.Ceiling(profitPerPerson)} liters per person.");
+}
+else if (wine < zLitters)
+{
+    Console.WriteLine($"It will be a tough winter! More {Math.Floor(diff)} liters wine needed.");
+}
