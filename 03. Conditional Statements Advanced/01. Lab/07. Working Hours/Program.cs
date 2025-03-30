e8dba00f4bda109ece6c260ea53a95e39ce39ef2
Dimitar-Peev <mctrix87@abv.bg>
2025-03-30 11:15:04 +0300

    Create "07. Working Hours"

---
+++ 03. Conditional Statements Advanced/01. Lab/07. Working Hours/Program.cs
@@ -0,0 +1,13 @@
+﻿int hour = int.Parse(Console.ReadLine());
+string dayOfWeek = Console.ReadLine();
+
+bool isClosed = hour < 10 || hour > 18 || ("Sunday").Equals(dayOfWeek);
+
+if (isClosed)
+{
+    Console.WriteLine("closed");
+}
+else
+{
+    Console.WriteLine("open");
+}
