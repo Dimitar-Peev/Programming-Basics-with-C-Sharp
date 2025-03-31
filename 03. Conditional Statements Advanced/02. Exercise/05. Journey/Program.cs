4e35e8a00dc7af8292d1d433ad63de992e9a281b
Dimitar-Peev <mctrix87@abv.bg>
2025-03-31 20:10:04 +0300

    Create "05. Journey"

---
+++ 03. Conditional Statements Advanced/02. Exercise/05. Journey/Program.cs
@@ -0,0 +1,48 @@
+﻿double budget = double.Parse(Console.ReadLine());
+string season = Console.ReadLine();
+
+double price = 0;
+string destination = "";
+
+if (budget <= 100)
+{
+    destination = "Bulgaria";
+    if ("summer".Equals(season))
+    {
+        price = budget * 0.3;
+    }
+    else if ("winter".Equals(season))
+    {
+        price = budget * 0.7;
+    }
+}
+else if (budget <= 1000)
+{
+    destination = "Balkans";
+    if ("summer".Equals(season))
+    {
+        price = budget * 0.4;
+    }
+    else if ("winter".Equals(season))
+    {
+        price = budget * 0.8;
+    }
+}
+else if (budget > 1000)
+{
+    destination = "Europe";
+    price = budget * 0.9;
+}
+
+string type = "";
+
+if ("winter".Equals(season) || destination.Equals("Europe"))
+{
+    type = "Hotel";
+}
+else
+{
+    type = "Camp";
+}
+Console.WriteLine($"Somewhere in {destination}");
+Console.WriteLine($"{type} - {price:F2}");
