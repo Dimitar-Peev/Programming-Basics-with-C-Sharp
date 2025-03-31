088fd890313c933ef58c7b6f06745fe8b00432a2
Dimitar-Peev <mctrix87@abv.bg>
2025-03-31 20:01:56 +0300

    Create "01. Cinema"

---
+++ 03. Conditional Statements Advanced/02. Exercise/01. Cinema/Program.cs
@@ -0,0 +1,24 @@
+﻿string projectionType = Console.ReadLine();
+int rowsCount = int.Parse(Console.ReadLine());
+int colsCount = int.Parse(Console.ReadLine());
+
+double premierePrice = 12;
+double normalPrice = 7.5;
+double discountPrice = 5;
+
+double income = 0;
+
+switch (projectionType)
+{
+    case "Premiere":
+        income = rowsCount * colsCount * premierePrice;
+        break;
+    case "Normal":
+        income = rowsCount * colsCount * normalPrice;
+        break;
+    case "Discount":
+        income = rowsCount * colsCount * discountPrice;
+        break;
+}
+
+Console.WriteLine($"{income:F2} leva");
