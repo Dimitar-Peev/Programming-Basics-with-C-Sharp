5c4a5266e598c5812fe9fa854f3131e0d646b5fa
Dimitar-Peev <mctrix87@abv.bg>
2025-03-31 20:03:23 +0300

    Create "02. Summer Outfit"

---
+++ 03. Conditional Statements Advanced/02. Exercise/02. Summer Outfit/Program.cs
@@ -0,0 +1,56 @@
+﻿int celsius = int.Parse(Console.ReadLine());
+string dayTime = Console.ReadLine();
+
+string outfit = "";
+string shoes = "";
+
+if (10 <= celsius && celsius <= 18)
+{
+    switch (dayTime)
+    {
+        case "Morning":
+            outfit = "Sweatshirt";
+            shoes = "Sneakers";
+            break;
+        case "Afternoon":
+        case "Evening":
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            break;
+    }
+}
+else if (18 < celsius && celsius <= 24)
+{
+    switch (dayTime)
+    {
+        case "Morning":
+        case "Evening":
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            break;
+        case "Afternoon":
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+            break;
+    }
+}
+else if (celsius >= 25)
+{
+    switch (dayTime)
+    {
+        case "Morning":
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+            break;
+        case "Afternoon":
+            outfit = "Swim Suit";
+            shoes = "Barefoot";
+            break;
+        case "Evening":
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            break;
+    }
+}
+
+Console.WriteLine($"It's {celsius} degrees, get your {outfit} and {shoes}.");
