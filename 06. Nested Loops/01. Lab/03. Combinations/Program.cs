b762a79bc580b27ddd3d6acdd5836950773b721f
Dimitar-Peev <mctrix87@abv.bg>
2025-04-01 17:50:28 +0300

    Create "03. Combinations"

---
+++ 06. Nested Loops/01. Lab/03. Combinations/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine());
+int count = 0;
+
+for (int first = 0; first <= n; first++)
+{
+
+    for (int second = 0; second <= n; second++)
+    {
+
+        for (int third = 0; third <= n; third++)
+        {
+
+            int sum = first + second + third;
+
+            if (sum == n)
+            {
+                count++;
+            }
+        }
+    }
+}
+
+Console.WriteLine(count);
