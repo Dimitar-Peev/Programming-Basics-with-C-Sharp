2b0e8505feb3afd0ab684bed7b5cae07eda4cb4a
Dimitar-Peev <mctrix87@abv.bg>
2025-04-01 18:06:04 +0300

    Create "05. Travelling"

---
+++ 06. Nested Loops/01. Lab/05. Travelling/Program.cs
@@ -0,0 +1,15 @@
+﻿string destination = Console.ReadLine();
+
+while (!"End".Equals(destination))
+{
+    double budget = double.Parse(Console.ReadLine());
+    double money = 0;
+
+    while (money < budget)
+    {
+        money += double.Parse(Console.ReadLine());
+    }
+    Console.WriteLine($"Going to {destination}!");
+
+    destination = Console.ReadLine();
+}
