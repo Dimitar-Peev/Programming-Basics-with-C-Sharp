4a7d2f7d5d2aa03744d7695882f0f638b28f49dc
Dimitar-Peev <mctrix87@abv.bg>
2025-04-02 20:29:17 +0300

    Create "10. Multiply by 2"

---
+++ 03. Conditional Statements Advanced/03. More Exercises/10. Multiply by 2/Program.cs
@@ -0,0 +1,15 @@
+﻿while (true)
+{
+    double number = double.Parse(Console.ReadLine());
+    if (number >= 0)
+    {
+        number *= 2;
+    }
+    else
+    {
+        Console.WriteLine("Negative number!");
+        break;
+    }
+
+    Console.WriteLine($"Result: {number:F2}");
+}
