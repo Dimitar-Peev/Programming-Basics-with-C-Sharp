6cc232bc10041041bdc351e3358afe6ad75f2b99
Dimitar-Peev <mctrix87@abv.bg>
2025-04-02 20:25:22 +0300

    Create "08. Point on Rectangle Border"

---
+++ 03. Conditional Statements Advanced/03. More Exercises/08. Point on Rectangle Border/Program.cs
@@ -0,0 +1,20 @@
+﻿double x1 = double.Parse(Console.ReadLine());
+double y1 = double.Parse(Console.ReadLine());
+double x2 = double.Parse(Console.ReadLine());
+double y2 = double.Parse(Console.ReadLine());
+double x = double.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+
+bool border1 = (x == x1) && (y >= y1) && (y <= y2);
+bool border2 = (x == x2) && (y >= y1) && (y <= y2);
+bool border3 = (y == y1) && (x >= x1) && (x <= x2);
+bool border4 = (y == y2) && (x >= x1) && (x <= x2);
+
+if (border1 || border2 || border3 || border4)
+{
+    Console.WriteLine("Border");
+}
+else
+{
+    Console.WriteLine("Inside / Outside");
+}
