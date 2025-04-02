d573e378dbbba83812eda448913f99b99f36d3f1
Dimitar-Peev <mctrix87@abv.bg>
2025-04-02 20:15:14 +0300

    Create "05. Vacation"

---
+++ 03. Conditional Statements Advanced/03. More Exercises/05. Vacation/Program.cs
@@ -0,0 +1,54 @@
+﻿double budget = double.Parse(Console.ReadLine());
+string season = Console.ReadLine();
+
+string location = "";
+string place = "";
+double price = 0;
+
+if (budget <= 1000)
+{
+    location = "Camp";
+    switch (season)
+    {
+        case "Summer":
+            place = "Alaska";
+            price = budget * 0.65;
+            break;
+        case "Winter":
+            place = "Morocco";
+            price = budget * 0.45;
+            break;
+    }
+}
+else if (budget <= 3000)
+{
+    location = "Hut";
+    switch (season)
+    {
+        case "Summer":
+            place = "Alaska";
+            price = budget * 0.8;
+            break;
+        case "Winter":
+            place = "Morocco";
+            price = budget * 0.6;
+            break;
+    }
+}
+else if (budget > 3000)
+{
+    location = "Hotel";
+    switch (season)
+    {
+        case "Summer":
+            place = "Alaska";
+            price = budget * 0.9;
+            break;
+        case "Winter":
+            place = "Morocco";
+            price = budget * 0.9;
+            break;
+    }
+}
+
+Console.WriteLine($"{place} - {location} - {price:F2}");
