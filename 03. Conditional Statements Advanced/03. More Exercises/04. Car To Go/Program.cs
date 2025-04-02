e889bf588690fe28acc9b3a40caacc1ab8b47a1a
Dimitar-Peev <mctrix87@abv.bg>
2025-04-02 20:12:02 +0300

    Create "04. Car To Go"

---
+++ 03. Conditional Statements Advanced/03. More Exercises/04. Car To Go/Program.cs
@@ -0,0 +1,48 @@
+﻿double budget = double.Parse(Console.ReadLine());
+string season = Console.ReadLine();
+
+string carClass = "";
+string carType = "";
+double carPrice = 0;
+
+if (budget <= 100)
+{
+    carClass = "Economy class";
+    switch (season)
+    {
+        case "Summer":
+            carType = "Cabrio";
+            carPrice = budget * 0.35;
+            break;
+        case "Winter":
+            carType = "Jeep";
+            carPrice = budget * 0.65;
+            break;
+    }
+
+}
+else if (budget <= 500)
+{
+    carClass = "Compact class";
+    switch (season)
+    {
+        case "Summer":
+            carType = "Cabrio";
+            carPrice = budget * 0.45;
+            break;
+        case "Winter":
+            carType = "Jeep";
+            carPrice = budget * 0.80;
+            break;
+    }
+}
+else
+{
+    carClass = "Luxury class";
+    carType = "Jeep";
+    carPrice = budget * 0.9;
+}
+
+
+Console.WriteLine(carClass);
+Console.WriteLine($"{carType} - {carPrice:F2}");
