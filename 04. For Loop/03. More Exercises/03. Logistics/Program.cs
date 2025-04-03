b5d48d3c89d787116e6ec3595b74a56adf2f27ae
Dimitar-Peev <mctrix87@abv.bg>
2025-04-03 17:29:01 +0300

    Create "03. Logistics"

---
+++ 04. For Loop/03. More Exercises/03. Logistics/Program.cs
@@ -0,0 +1,39 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+double totalTons = 0;
+
+double tonVan = 0;
+double tonTruck = 0;
+double tonTrain = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int ton = int.Parse(Console.ReadLine());
+    totalTons += ton;
+
+    if (ton <= 3)
+    {
+        tonVan += ton;
+    }
+    else if (ton <= 11)
+    {
+        tonTruck += ton;
+    }
+    else
+    {
+        tonTrain += ton;
+    }
+
+}
+
+double averagePrice = ((tonVan * 200) + (tonTruck * 175) + (tonTrain * 120)) / totalTons;
+Console.WriteLine($"{averagePrice:F2}", averagePrice);
+
+double tonVanPercent = (tonVan / totalTons) * 100;
+Console.WriteLine($"{tonVanPercent:F2}%");
+
+double tonTruckPercent = (tonTruck / totalTons) * 100;
+Console.WriteLine($"{tonTruckPercent:F2}%");
+
+double tonTrainPercent = (tonTrain / totalTons) * 100;
+Console.WriteLine($"{tonTrainPercent:F2}%");
