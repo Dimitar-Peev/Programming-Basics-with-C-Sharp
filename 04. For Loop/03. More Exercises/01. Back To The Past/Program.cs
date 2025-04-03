15a02c3c97d039e602de1f4a6fe067a5982c32f8
Dimitar-Peev <mctrix87@abv.bg>
2025-04-03 17:22:55 +0300

    Create "01. Back To The Past"

---
+++ 04. For Loop/03. More Exercises/01. Back To The Past/Program.cs
@@ -0,0 +1,28 @@
+﻿double money = double.Parse(Console.ReadLine());
+int year = int.Parse(Console.ReadLine());
+
+int age = 18;
+int diff = year - 1800;
+
+for (int i = 0; i <= diff; i++)
+{
+    if (i % 2 == 0)
+    {
+        money -= 12000;
+    }
+    else
+    {
+        money = money - (12000 + (50 * age));
+    }
+
+    age++;
+}
+
+if (money >= 0)
+{
+    Console.WriteLine($"Yes! He will live a carefree life and will have {money:F2} dollars left.");
+}
+else
+{
+    Console.WriteLine($"He will need {Math.Abs(money):F2} dollars to survive.");
+}
