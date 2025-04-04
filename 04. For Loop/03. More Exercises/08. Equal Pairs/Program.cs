ef1a500b43a1c50fc1406fb8a755480e1f29c879
Dimitar-Peev <mctrix87@abv.bg>
2025-04-04 17:38:57 +0300

    Create "08. Equal Pairs"

---
+++ 04. For Loop/03. More Exercises/08. Equal Pairs/Program.cs
@@ -0,0 +1,32 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int firstSum = 0;
+int secondSum = 0;
+int diff = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    int n1 = int.Parse(Console.ReadLine());
+    int n2 = int.Parse(Console.ReadLine());
+
+    if (i % 2 != 0)
+    {
+        firstSum = n1 + n2;
+    }
+    else
+    {
+        secondSum = n1 + n2;
+    }
+
+}
+
+diff = Math.Abs(firstSum - secondSum);
+
+if (firstSum == secondSum || n < 2)
+{
+    Console.WriteLine($"Yes, value={firstSum}");
+}
+else
+{
+    Console.WriteLine($"No, maxdiff={diff}");
+}
