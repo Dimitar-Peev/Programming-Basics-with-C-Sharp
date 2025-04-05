e660c9136ad64e7981c6b3d455552a1e203cf6dc
Dimitar-Peev <mctrix87@abv.bg>
2025-04-05 21:51:44 +0300

    Create "04. Train The Trainers"

---
+++ 06. Nested Loops/02. Exercise/04. Train The Trainers/Program.cs
@@ -0,0 +1,28 @@
+﻿int juryCount = int.Parse(Console.ReadLine());
+
+int presentationsCount = 0;
+double allGradesSum = 0;
+double currentGradesSum = 0;
+string presentationName = Console.ReadLine();
+
+while (presentationName != "Finish")
+{
+    presentationsCount++;
+
+    for (int i = 1; i <= juryCount; i++)
+    {
+        double grade = double.Parse(Console.ReadLine());
+        currentGradesSum += grade;
+        allGradesSum += grade;
+    }
+
+    double averageGrade = currentGradesSum / juryCount;
+    currentGradesSum = 0;
+
+    Console.WriteLine($"{presentationName} - {averageGrade:f2}.");
+
+    presentationName = Console.ReadLine();
+}
+
+double averageGradeAll = allGradesSum / (presentationsCount * juryCount);
+Console.WriteLine($"Student's final assessment is {averageGradeAll:f2}.");
