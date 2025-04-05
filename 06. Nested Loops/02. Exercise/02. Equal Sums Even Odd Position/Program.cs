78abb2e933a2923e8d3572b5ad7781cf4530d1bc
Dimitar-Peev <mctrix87@abv.bg>
2025-04-05 21:40:14 +0300

    Create "02. Equal Sums Even Odd Position"

---
+++ 06. Nested Loops/02. Exercise/02. Equal Sums Even Odd Position/Program.cs
@@ -0,0 +1,30 @@
+﻿int firstNum = int.Parse(Console.ReadLine());
+int secondNum = int.Parse(Console.ReadLine());
+
+for (int currentNum = firstNum; currentNum <= secondNum; currentNum++)
+{
+    string currentNumAsString = currentNum.ToString();
+    int evenNumsSum = 0;
+    int oddNumsSum = 0;
+
+
+    int length = currentNumAsString.Length;
+    for (int digitPosition = 0; digitPosition < length; digitPosition++)
+    {
+        int currentDigit = int.Parse(currentNumAsString[digitPosition].ToString());
+
+        if (digitPosition % 2 == 0)
+        {
+            evenNumsSum += currentDigit;
+        }
+        else
+        {
+            oddNumsSum += currentDigit;
+        }
+    }
+
+    if (evenNumsSum == oddNumsSum)
+    {
+        Console.Write(currentNum + " ");
+    }
+}
