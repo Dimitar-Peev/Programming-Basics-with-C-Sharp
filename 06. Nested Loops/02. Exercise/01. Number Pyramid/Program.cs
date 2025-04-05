4e031daf6f237c6b8abd10d82f51ed56a7a830c0
Dimitar-Peev <mctrix87@abv.bg>
2025-04-05 21:37:54 +0300

    Create "01. Number Pyramid"

---
+++ 06. Nested Loops/02. Exercise/01. Number Pyramid/Program.cs
@@ -0,0 +1,25 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int currentNumber = 0;
+
+for (int row = 1; row <= n; row++)
+{
+    for (int col = 1; col <= row; col++)
+    {
+        currentNumber++;
+
+        if (currentNumber > n)
+        {
+            break;
+        }
+
+        Console.Write(currentNumber + " ");
+    }
+
+    if (currentNumber > n)
+    {
+        break;
+    }
+
+    Console.WriteLine();
+}
