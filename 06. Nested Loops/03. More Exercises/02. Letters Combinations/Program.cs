43f8b3181767fab92a5e0b4c129980c2c7be6df6
Dimitar-Peev <mctrix87@abv.bg>
2025-04-06 20:40:55 +0300

    Create "02. Letters Combinations"

---
+++ 06. Nested Loops/03. More Exercises/02. Letters Combinations/Program.cs
@@ -0,0 +1,22 @@
+﻿char ch1 = char.Parse(Console.ReadLine());
+char ch2 = char.Parse(Console.ReadLine());
+char ch3 = char.Parse(Console.ReadLine());
+
+int counter = 0;
+
+for (char i = ch1; i <= ch2; i++)
+{
+    for (char j = ch1; j <= ch2; j++)
+    {
+        for (char k = ch1; k <= ch2; k++)
+        {
+            if (i != ch3 && j != ch3 && k != ch3)
+            {
+                Console.Write($"{i}{j}{k} ");
+                counter++;
+            }
+        }
+    }
+}
+
+Console.WriteLine(counter);
