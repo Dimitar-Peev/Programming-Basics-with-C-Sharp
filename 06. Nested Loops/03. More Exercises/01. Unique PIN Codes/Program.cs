b5cf848ed41307f3835453e914a66b1f535caa2c
Dimitar-Peev <mctrix87@abv.bg>
2025-04-06 20:35:05 +0300

    Create "01. Unique PIN Codes"

---
+++ 06. Nested Loops/03. More Exercises/01. Unique PIN Codes/Program.cs
@@ -0,0 +1,17 @@
+﻿int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= number1; i += 2)
+{
+    for (int j = 1; j <= number2; j++)
+    {
+        if (j == 2 || j == 3 || j == 5 || j == 7)
+        {
+            for (int k = 2; k <= number3; k += 2)
+            {
+                Console.WriteLine($"{i} {j} {k}");
+            }
+        }
+    }
+}
