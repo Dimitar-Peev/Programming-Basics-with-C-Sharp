8121db4d6ac947ce144944f66591ea5124317f0d
Dimitar-Peev <mctrix87@abv.bg>
2025-04-06 20:59:01 +0300

    Create "05. Challenge the Wedding"

---
+++ 06. Nested Loops/03. More Exercises/05. Challenge the Wedding/Program.cs
@@ -0,0 +1,19 @@
+﻿int menCount = int.Parse(Console.ReadLine());
+int womenCount = int.Parse(Console.ReadLine());
+int maxTables = int.Parse(Console.ReadLine());
+
+int tablesUsed = 0;
+
+for (int man = 1; man <= menCount; man++)
+{
+    for (int woman = 1; woman <= womenCount; woman++)
+    {
+        Console.Write($"({man} <-> {woman}) ");
+        tablesUsed++;
+
+        if (tablesUsed >= maxTables)
+        {
+            return;
+        }
+    }
+}
