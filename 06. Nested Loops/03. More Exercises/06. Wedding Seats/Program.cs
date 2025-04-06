c15d3a7bffd51fa799c0b8808613efb889324557
Dimitar-Peev <mctrix87@abv.bg>
2025-04-06 21:02:52 +0300

    Create "06. Wedding Seats"

---
+++ 06. Nested Loops/03. More Exercises/06. Wedding Seats/Program.cs
@@ -0,0 +1,23 @@
+﻿char lastSector = char.Parse(Console.ReadLine());
+int rowsInFirstSector = int.Parse(Console.ReadLine());
+int seatsOnOddRow = int.Parse(Console.ReadLine());
+
+int totalSeats = 0;
+
+for (char sector = 'A'; sector <= lastSector; sector++)
+{
+    int currentSectorRows = rowsInFirstSector + (sector - 'A');
+
+    for (int row = 1; row <= currentSectorRows; row++)
+    {
+        int seats = row % 2 == 0 ? seatsOnOddRow + 2 : seatsOnOddRow;
+
+        for (char seat = 'a'; seat < 'a' + seats; seat++)
+        {
+            Console.WriteLine($"{sector}{row}{seat}");
+            totalSeats++;
+        }
+    }
+}
+
+Console.WriteLine(totalSeats);
