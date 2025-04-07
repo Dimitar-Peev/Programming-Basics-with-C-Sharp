1baa238daa57b93d4e892e9bc6235c5afeeacfdc
Dimitar-Peev <mctrix87@abv.bg>
2025-04-07 21:37:08 +0300

    Create "05. Square Frame"

---
+++ Drawing Figures with Loops/05. Square Frame/Program.cs
@@ -0,0 +1,25 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Console.Write("+ ");
+for (int dashes = 1; dashes <= n - 2; dashes++)
+{
+    Console.Write("- ");
+}
+Console.WriteLine("+");
+
+for (int row = 1; row <= n - 2; row++)
+{
+    Console.Write("| ");
+    for (int dashes = 1; dashes <= n - 2; dashes++)
+    {
+        Console.Write("- ");
+    }
+    Console.WriteLine("|");
+}
+Console.Write("+ ");
+
+for (int dashes = 1; dashes <= n - 2; dashes++)
+{
+    Console.Write("- ");
+}
+Console.WriteLine("+");
