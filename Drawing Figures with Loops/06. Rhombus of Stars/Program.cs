54d5980cbc446ca254883024ccde8b762f1a5375
Dimitar-Peev <mctrix87@abv.bg>
2025-04-08 22:01:32 +0300

    Create "06. Rhombus of Stars"

---
+++ Drawing Figures with Loops/06. Rhombus of Stars/Program.cs
@@ -0,0 +1,27 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int row = 1; row <= n; row++)
+{
+    Console.Write(new string(' ', n - row));
+    Console.Write("*");
+
+    for (int i = 1; i < row; i++)
+    {
+        Console.Write(" *");
+    }
+
+    Console.WriteLine();
+}
+
+for (int row = n - 1; row >= 1; row--)
+{
+    Console.Write(new string(' ', n - row));
+    Console.Write("*");
+
+    for (int i = 1; i < row; i++)
+    {
+        Console.Write(" *");
+    }
+
+    Console.WriteLine();
+}
