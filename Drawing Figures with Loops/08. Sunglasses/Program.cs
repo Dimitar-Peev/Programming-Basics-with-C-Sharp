0d01206ca0d7ec50e164e61f70601ab9fd9b78c4
Dimitar-Peev <mctrix87@abv.bg>
2025-04-08 22:14:33 +0300

    Create "08. Sunglasses"

---
+++ Drawing Figures with Loops/08. Sunglasses/Program.cs
@@ -0,0 +1,69 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n * 2; i++)
+{
+    Console.Write("*");
+}
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write(" ");
+}
+
+for (int i = 0; i < n * 2; i++)
+{
+    Console.Write("*");
+}
+
+Console.WriteLine();
+
+for (int row = 1; row <= n - 2; row++)
+{
+    Console.Write("*");
+    for (int i = 0; i < (n * 2) - 2; i++)
+    {
+        Console.Write("/");
+    }
+    Console.Write("*");
+
+    if (row == (n - 1) / 2)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("|");
+        }
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(" ");
+        }
+    }
+
+    Console.Write("*");
+    for (int i = 0; i < (n * 2) - 2; i++)
+    {
+        Console.Write("/");
+    }
+    Console.Write("*");
+
+    Console.WriteLine();
+}
+
+for (int i = 0; i < n * 2; i++)
+{
+    Console.Write("*");
+}
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write(" ");
+}
+
+for (int i = 0; i < n * 2; i++)
+{
+    Console.Write("*");
+}
+
+Console.WriteLine();
