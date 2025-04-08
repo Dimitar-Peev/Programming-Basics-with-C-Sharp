f04f906cfa60a2ef05f99de51f69e8ee7d33ee84
Dimitar-Peev <mctrix87@abv.bg>
2025-04-08 22:17:11 +0300

    Create "09. House"

---
+++ Drawing Figures with Loops/09. House/Program.cs
@@ -0,0 +1,67 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int dashesEven = (n - 2) / 2;
+int dashesOdd = (n - 1) / 2;
+int starsEven = 1;
+int starsOdd = 1;
+
+int roof = (n + 1) / 2;
+
+for (int rowRoof = 1; rowRoof <= roof; rowRoof++)
+{
+    if (n % 2 == 0)
+    {
+        for (int i = 0; i < dashesEven; i++)
+        {
+            Console.Write("-");
+        }
+
+        for (int i = 0; i < starsEven * 2; i++)
+        {
+            Console.Write("*");
+        }
+
+        for (int i = 0; i < dashesEven; i++)
+        {
+            Console.Write("-");
+        }
+
+        Console.WriteLine();
+        dashesEven--;
+        starsEven++;
+    }
+    else
+    {
+        for (int i = 0; i < dashesOdd; i++)
+        {
+            Console.Write("-");
+        }
+
+        for (int i = 0; i < starsOdd; i++)
+        {
+            Console.Write("*");
+        }
+
+        for (int i = 0; i < dashesOdd; i++)
+        {
+            Console.Write("-");
+        }
+
+        Console.WriteLine();
+        starsOdd += 2;
+        dashesOdd--;
+    }
+}
+
+int baseHeight = n / 2;
+
+for (int rowBase = 1; rowBase <= baseHeight; rowBase++)
+{
+    Console.Write("|");
+    for (int i = 0; i < n - 2; i++)
+    {
+        Console.Write("*");
+    }
+
+    Console.WriteLine("|");
+}
